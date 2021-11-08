8227dc2849260d423ae0e955ebe1ce94e4468a30
claudiopedalino <claudio.dpedalino@gmail.com>
2021-11-08 08:48:25 -0300

    ~ Fix Readme ~ ~

---
+++ Sat.Recruitment.Infra/Migrations/20211108100443_test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sat.Recruitment.Infra.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("086e013d-f9fc-464f-b939-e370e3945d1c"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("b079b96b-0cd5-4dd2-ae81-46f402827962"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("ba6f8779-b4f2-4a8c-a0d8-2a4b64324d50"));
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "CreateBy", "DeleteBy", "Email", "IsDelete", "LastModificationAt", "Money", "Name", "Phone", "UpdateBy", "UserType" },
+                values: new object[] { new Guid("2dfd02a5-eb3e-4d99-ac6a-663bc2ad766f"), "Peru 2464", "Seed Initial Data", null, "Juan@marmol.com", false, new DateTime(2021, 11, 8, 7, 4, 43, 149, DateTimeKind.Unspecified).AddTicks(6138), 1234m, "Juan", "+5491154762312", null, "Normal" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "CreateBy", "DeleteBy", "Email", "IsDelete", "LastModificationAt", "Money", "Name", "Phone", "UpdateBy", "UserType" },
+                values: new object[] { new Guid("2479547f-4a43-4b1f-b3eb-69c8aa2ea093"), "Alvear y Colombres", "Seed Initial Data", null, "Franco.Perez@gmail.com", false, new DateTime(2021, 11, 8, 7, 4, 43, 151, DateTimeKind.Unspecified).AddTicks(8658), 112234m, "Franco", "+534645213542", null, "Premium" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "CreateBy", "DeleteBy", "Email", "IsDelete", "LastModificationAt", "Money", "Name", "Phone", "UpdateBy", "UserType" },
+                values: new object[] { new Guid("351dad25-8516-414d-b656-dac18b1982d5"), "Garay y Otra Calle", "Seed Initial Data", null, "Agustina@gmail.com", false, new DateTime(2021, 11, 8, 7, 4, 43, 151, DateTimeKind.Unspecified).AddTicks(8909), 112234m, "Agustina", "+534645213542", null, "SuperUser" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("2479547f-4a43-4b1f-b3eb-69c8aa2ea093"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("2dfd02a5-eb3e-4d99-ac6a-663bc2ad766f"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("351dad25-8516-414d-b656-dac18b1982d5"));
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "CreateBy", "DeleteBy", "Email", "IsDelete", "LastModificationAt", "Money", "Name", "Phone", "UpdateBy", "UserType" },
+                values: new object[] { new Guid("ba6f8779-b4f2-4a8c-a0d8-2a4b64324d50"), "Peru 2464", "Seed Initial Data", null, "Juan@marmol.com", false, new DateTime(2021, 11, 7, 16, 59, 3, 385, DateTimeKind.Unspecified).AddTicks(3758), 1234m, "Juan", "+5491154762312", null, "Normal" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "CreateBy", "DeleteBy", "Email", "IsDelete", "LastModificationAt", "Money", "Name", "Phone", "UpdateBy", "UserType" },
+                values: new object[] { new Guid("b079b96b-0cd5-4dd2-ae81-46f402827962"), "Alvear y Colombres", "Seed Initial Data", null, "Franco.Perez@gmail.com", false, new DateTime(2021, 11, 7, 16, 59, 3, 387, DateTimeKind.Unspecified).AddTicks(5735), 112234m, "Franco", "+534645213542", null, "Premium" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "CreateBy", "DeleteBy", "Email", "IsDelete", "LastModificationAt", "Money", "Name", "Phone", "UpdateBy", "UserType" },
+                values: new object[] { new Guid("086e013d-f9fc-464f-b939-e370e3945d1c"), "Garay y Otra Calle", "Seed Initial Data", null, "Agustina@gmail.com", false, new DateTime(2021, 11, 7, 16, 59, 3, 387, DateTimeKind.Unspecified).AddTicks(5972), 112234m, "Agustina", "+534645213542", null, "SuperUser" });
+        }
+    }
+}
